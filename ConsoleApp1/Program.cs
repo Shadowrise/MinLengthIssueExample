27ce84443a26e0c11d3b6131d1e84ec5d3620a1b
Емцов Вячеслав Сергеевич <emtsov.vs@esphere.ru>
2020-08-05 18:43:54 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using test;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating a valid Test object
+            // minOccurs = 0, maxOccurs = 2
+            var test = new Test();
+            test.El.Add("0123456789");
+            test.El.Add("0123456789");
+
+            var context = new ValidationContext(test);
+            var errors = new List<ValidationResult>();
+            Validator.TryValidateObject(test, context, errors, true);
+
+            Console.WriteLine("Errors count: " + errors.Count);
+            foreach (var error in errors)
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/_Generate.bat
@@ -0,0 +1,9 @@
+"C:\Users\emtsov.vs\Documents\Source\Repos\backend\XmlSchemaClassGenerator.Console\bin\Debug\net461\XmlSchemaClassGenerator.Console.2.0.444.exe" ^
+--order ^
+--description=false ^
+--debuggerStepThrough=false ^
+--separateFiles ^
+-n "|test.xsd=test" ^
+"test.xsd"
+
+pause
